65968939f497b221ffa6d50044e631213c60c1e1
JFelipeGuillenM <jose.guillenm@ug.edu.ec>
2023-12-17 12:50:31 -0500

    Corrección en main.py

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 from config.config import *
-import os
-import json
 from src.chat import *
 import telebot
 from flask import Flask, request
 import time
 from waitress import serve
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 #instancia del bot
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 #intancia del servidor de Flask
 web_server = Flask(__name__)
-
-# Cargar el modelo y datos del chat
-json_path = os.path.join('training_data', 'training.json')
-intents = json.loads(open(json_path, encoding='utf-8').read())
 
 #Gestiona las pesticones POST enviadas el servidor
 @web_server.route('/', methods = ['POST'])
@@ -44,7 +34,7 @@ def bot_texto(message):
   ints = predict_class(message.text)
   res = get_response(ints, intents)
   
-  bot.send_message(message.chat.id, res, parse_mode="html")
+  bot.send_message(message.chat.id, res.text, parse_mode="html")
 
 #main------------------------------------------------------------------------------------
 if __name__ == '__main__':
