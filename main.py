87bd9155f4f628b519cdc2a967f9c6ad1cd18cf0
JFelipeGuillenM <jose.guillenm@ug.edu.ec>
2023-12-16 22:14:29 -0500

    uso del modelo de ia en main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from config.config import *
+import os
+import json
+from src.chat import predict_class, get_response
 import telebot
 from flask import Flask, request
 import time
@@ -8,6 +11,10 @@ from waitress import serve
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 #intancia del servidor de Flask
 web_server = Flask(__name__)
+
+# Cargar el modelo y datos del chat
+json_path = os.path.join('training_data', 'training.json')
+intents = json.loads(open(json_path, encoding='utf-8').read())
 
 #Gestiona las pesticones POST enviadas el servidor
 @web_server.route('/', methods = ['POST'])
@@ -23,9 +30,23 @@ def cmd_start(message):
     bot.send_message(message.chat.id, "Hola", parse_mode = "html")
 
 #Gestión de mensajes
-@bot.message_handler(content_types = ['text'])
+#@bot.message_handler(content_types = ['text'])
+#def bot_texto(message):
+#    bot.send_message(message.chat.id, message.text, parse_mode = "html")
+
+# Gestión de mensajes
+@bot.message_handler(content_types=['text'])
 def bot_texto(message):
-    bot.send_message(message.chat.id, message.text, parse_mode = "html")
+    # Obtener la respuesta del chat
+    response = get_chat_response(message.text)
+    # Enviar la respuesta al usuario
+    bot.send_message(message.chat.id, response)
+
+def get_chat_response(message):
+    # Obtener la respuesta del modelo de chat
+    intents_list = predict_class(message)
+    response = get_response(intents_list, intents)
+    return response
 
 #main------------------------------------------------------------------------------------
 if __name__ == '__main__':
