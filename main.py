5e89c72f8e2e2a362f6ccf42d908fb13548fd662
JFelipeGuillenM <jose.guillenm@ug.edu.ec>
2023-12-17 15:47:39 -0500

    descarga de punkt de nltk en main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import pickle
 import numpy as np
 from tensorflow import keras
 import nltk
+nltk.download('punkt')
 from nltk.stem import WordNetLemmatizer
 import h5py
 from keras.models import load_model
