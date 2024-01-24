adc14139739459ae95831570c3d07fc6801e675b
JFelipeGuillenM <jose.guillenm@ug.edu.ec>
2024-01-24 00:42:44 -0500

    procedimientos

--- src/user/user_interactions.py
+++ src/user/user_interactions.py
@@ -95,7 +95,7 @@ def create_interaction_entry(user_id):
         conn = connect_to_db()
         cur = conn.cursor()
         # Llama al procedimiento almacenado
-        cur.callproc('create_interaction_entry', [user_id])
+        cur.execute('create_interaction_entry', (user_id,))
         # Confirma los cambios
         conn.commit()
         cur.close()
