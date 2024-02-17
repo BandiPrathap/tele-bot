b2285807ba0a760371c015dc9cdc87b9176d00cd
JFelipeGuillenM <jose.guillenm@ug.edu.ec>
2024-02-17 13:46:17 -0500

    Correción de compatibilidad en construcción del modelo

--- src/model/model_creation.py
+++ src/model/model_creation.py
@@ -1,45 +1,55 @@
+# Importaciones de bibliotecas necesarias
 import os
 import random
 import json
 import pickle
 import numpy as np
 import tensorflow as tf
-
-# Importando NLTK y el lematizador de palabras
+from tensorflow import keras
+from keras.regularizers import l2
 import nltk
+nltk.download('punkt') # Descarga de datos necesarios para la tokenización
+nltk.download('wordnet')  # Descarga de datos para el lematizaxdor
 from nltk.stem import WordNetLemmatizer
+import unicodedata
+from sklearn.model_selection import StratifiedKFold
+from collections import defaultdict
 
-# Ruta de archivos
-words_path = os.path.join('data', 'words.pkl')
-classes_path = os.path.join('data', 'classes.pkl')
-json_path = os.path.join('training_data', 'training_joto.json')
-model_path = os.path.join('models','chat_model_val.h5')
-
-# Inicializando el lematizador
-lemmatizer = WordNetLemmatizer()
-
-# Cargando los datos de entrenamiento desde 'training.json' en la variable 'intents'
-with open(json_path, 'r') as json_file:
+# Rutas para guardar y cargar archivos como listas de palabras, clases, datos de entrenamiento y modelo
+words_path = os.path.join('data','words.pkl')
+classes_path = os.path.join('data','classes.pkl')
+json_path = os.path.join('training_data','training.json')
+model_path = os.path.join('models','chat_model.h5')
+# Carga de los datos de entrenamiento desde 'training.json'
+with open(json_path, 'r', encoding='utf-8') as json_file:
     intents = json.load(json_file)
 
-# Inicializando listas y variables para procesar las palabras y clases
-words = [] # Almacena todas las palabras 
-classes = [] # Almacena todas las clases 
+# Elimina tildes del texto
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return "".join([c for c in nfkd_form if not unicodedata.combining(c)])
+
+# Inicializa el lematizador
+lemmatizer = WordNetLemmatizer()
+
+# Inicializa listas y variables para procesar las palabras y clases
+words = [] # Almacena todas las palabras
+classes = [] # Almacena todas las clases
 documents = [] # Almacena las palabras con su respectivo identificador o clase
 ignoreLetters = ['¿','?','¡','!','.',',',';'] # Caracteres a ignorar
 
-# Recorriendo los datos del json para rellenar las listas
+# Recorre los datos del json para rellenar las listas
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         # Tokeniza el patrón en palabras
         wordList = nltk.word_tokenize(pattern)
         words.extend(wordList) # Agrega las palabras a la lista 'words'
         documents.append((wordList, intent['tag'])) # Agrega las palabras y su clase a 'documents'
-        if intent['tag'] not in classes: 
+        if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# Lematizando y filtrando las palabras, para ordenarlas y eliminar duplicados
-words = [lemmatizer.lemmatize(word) for word in words if word not in ignoreLetters]
+# Lematizando, filtrando las palabras y quitando tildes para ordenarlas y eliminar duplicados
+words = [lemmatizer.lemmatize(remove_accents(word).lower()) for word in words if word not in ignoreLetters]
 words = sorted(set(words))
 classes = sorted(set(classes))
 
@@ -59,35 +69,108 @@ for document in documents:
     # Crea un bag de palabras binarias
     for word in words:
         bag.append(1) if word in wordPatterns else bag.append(0)
-    # Se crea una lista de salida categórica
+    # Crea una lista de salida categórica
     outputRow = list(outputEmpty)
     outputRow[classes.index(document[1])] = 1
-    # Se combina el bag de palabras con la lista de salida y se agregan a la lista 'training'
+    # Combina el bag de palabras con la lista de salida y se agregan a la lista 'training'
     training.append(bag + outputRow)
 
-# Mezclando aleatoriamente los datos de entrenamiento
+# Mezclan aleatoriamente los datos de entrenamiento
 random.shuffle(training)
 # Convierte 'training' a una matriz NumPy
 training = np.array(training)
 
-# Dividiendo 'training' en matrices de entrada ('trainX') y salida ('trainY')
+# Divide 'training' en matrices de entrada ('trainX') y salida ('trainY')
 trainX = training[:, :len(words)]
 trainY = training[:, len(words):]
 
-# Modelo de red neuronal con 128 neuronas 
-model = tf.keras.Sequential()
-model.add(tf.keras.layers.Dense(128, input_shape=(len(trainX[0]),), activation = 'relu'))
-model.add(tf.keras.layers.Dropout(0.5))
-model.add(tf.keras.layers.Dense(64, activation = 'relu'))
-model.add(tf.keras.layers.Dropout(0.5))
-model.add(tf.keras.layers.Dense(len(trainY[0]), activation='softmax'))
+# Agrupa los documentos por tag
+grouped_documents = {}
+for document in documents:
+    tag = document[1]
+    if tag not in grouped_documents:
+        grouped_documents[tag] = []
+    grouped_documents[tag].append(document)
 
-# Configura el optimizador y compila el modelo
-sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+num_folds = 5
 
-# Entrena y guarda el modelo
-model.fit(trainX, trainY, epochs=250, batch_size=5, verbose=1)
+def divide_in_folds(documents, num_folds):
+    folds = [[] for _ in range(num_folds)]
+    for i, document in enumerate(documents):
+        folds[i % num_folds].append(document)
+    return folds
+
+# Aplica esta función a cada grupo de documentos
+grouped_folds = {tag: divide_in_folds(grouped_documents[tag], num_folds) for tag in grouped_documents}
+
+# Construye los folds para la validación cruzada
+folds = []
+for i in range(num_folds):
+    fold_documents = []
+    for tag in grouped_folds:
+        fold_documents.extend(grouped_folds[tag][i])
+    folds.append(fold_documents)
+
+ # Convierte los folds en formato adecuado para entrenamiento y prueba
+fold_data = []
+for fold in folds:
+    training = []
+    for document in fold:
+        bag = [1 if word in document[0] else 0 for word in words]
+        output_row = [0] * len(classes)
+        output_row[classes.index(document[1])] = 1
+        training.append(bag + output_row)
+    training = np.array(training)
+    fold_data.append((training[:, :len(words)], training[:, len(words):]))
+
+# Inicializa las métricas de rendimiento
+acc_per_fold = []
+loss_per_fold = []
+
+# Validación Cruzada
+for fold_no in range(num_folds):
+    # Prepara los datos de entrenamiento y prueba para la iteración actual
+    train_data = [data for i, data in enumerate(fold_data) if i != fold_no]
+    test_data = fold_data[fold_no]
+
+    trainX = np.vstack([data[0] for data in train_data])
+    trainY = np.vstack([data[1] for data in train_data])
+    testX, testY = test_data
+
+    #creación del modelo
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(256, input_shape=(len(trainX[0]),), activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(len(trainY[0]), activation='softmax')
+    ])
+
+    # Compilación del modelo con función de pérdida y métricas
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # Entrenamiento del modelo
+    model.fit(trainX, trainY, epochs=250, batch_size=5, verbose=1)  # Ajusta según sea necesario
+
+    # Evaluación del modelo en el conjunto de prueba
+    scores = model.evaluate(testX, testY, verbose=0)
+    print(f'Score for fold {fold_no+1}: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
+    acc_per_fold.append(scores[1] * 100)
+    loss_per_fold.append(scores[0])
+
+# Muestra el rendimiento promedio
+print('------------------------------------------------------------------------')
+print('Puntuación por fold')
+for i in range(0, len(acc_per_fold)):
+    print(f'> Fold {i+1} - Loss: {loss_per_fold[i]} - Accuracy: {acc_per_fold[i]}%')
+print('------------------------------------------------------------------------')
+print('Promedio de todas las iteraciones:')
+print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
+print(f'> Loss: {np.mean(loss_per_fold)}')
+
+#guarda el modelo en la ruta epecificada
+
 model.save(model_path)
-
 print('Done')
